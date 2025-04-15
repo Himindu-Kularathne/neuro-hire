8d1eedef4318d364728aea78022d720e94feff5c
Min3th <minethweerasinghe@gmail.com>
2025-04-15 18:52:05 +0530

    add tailwind to front end

--- gdrive/main.py
+++ gdrive/main.py
@@ -3,15 +3,18 @@ from quickstart import create_folder,upload_file;
 
 app = FastAPI()
 
+# Main function to indicate that service is running
 @app.get("/")
 def root():
     return {"message":"Google drive service is running"}
 
+# Creates folder for a given name
 @app.post("/create-folder")
 def create_drive_folder(folder_name:str = Form(...)):
     folder_id = create_folder(folder_name)
     return {"folderId": folder_id}
 
+# Uploads given file
 @app.post("/upload-cv")
 def upload_cv(file:UploadFile = File(...)):
     file_id = upload_file(file)
