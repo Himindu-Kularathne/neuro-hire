4cc147286a1476472d5d8741f363135c55627d75
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2025-04-19 19:17:53 +0530

    Add CORS support in main service and fix token extraction in auth middleware

--- services/main-service/middlewares/authMiddleware.js
+++ services/main-service/middlewares/authMiddleware.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader?.split(' ')[2];
+  const token = authHeader?.split(' ')[1];
 
   if (!token) return res.status(401).json({ message: 'Access token required' });
 
+  console.log("Token from header:", token);
+  console.log("JWT Secret:", process.env.JWT_SECRET);
   jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+    console.log("Payload from token:", err);
     if (err) return res.status(403).json({ message: 'Invalid or expired token' });
+    console.log("Decoded payload:", payload);
     req.user = payload.user;
     next();
   });
