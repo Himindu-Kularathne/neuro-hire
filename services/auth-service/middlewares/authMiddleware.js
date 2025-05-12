45735710b86f144663f12593fa73cba60fd6afe8
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2025-05-12 19:33:15 +0530

    fix: Correct token extraction in auth middleware to use the proper index

--- services/auth-service/middlewares/authMiddleware.js
+++ services/auth-service/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const { verifyToken } = require("../utils/jwt");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[2];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) return res.status(401).json({ message: "No token provided" });
 
