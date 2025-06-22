4de9641409ccaffa80fac86f5df2b21aa81a390b
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2025-06-22 20:42:26 +0530

    feat: add MIT License header to all source files

--- services/main-service/middlewares/errorHandler.js
+++ services/main-service/middlewares/errorHandler.js
@@ -1,6 +1,13 @@
+/*
+Copyright (c) 2025 Neuro Hire
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+*/
+
 const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: `Something went wrong! : ${err}` });
-  };
-  
+  console.error(err.stack);
+  res.status(500).json({ error: `Something went wrong! : ${err}` });
+};
+
 module.exports = errorHandler;
